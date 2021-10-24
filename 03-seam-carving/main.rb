f7352707a0efe46fffb83f72a7be9230dea71673
anvox <thien.an.vo.nguyen@gmail.com>
2021-10-24 15:13:35 +0700

    Package energy calculator

--- 03-seam-carving/main.rb
+++ 03-seam-carving/main.rb
@@ -6,11 +6,11 @@ filename = ARGV[0]
 direction = ARGV[1] || 'vertical'
 
 pixels = read_image_into_array("#{filename}.jpg")
-energies, max_energy = compute_energy(pixels)
-vertical_seam = compute_vertical_seam(energies)
 
-energy_colors = energy_to_colors(energies, max_energy)
-write_array_into_image(energy_colors, "#{filename}-energy.jpg")
+image_energy = Energy.new(pixels)
+vertical_seam = compute_vertical_seam(image_energy.energies)
+
+write_array_into_image(image_energy.to_colors, "#{filename}-energy.jpg")
 
 highlight_pixels = highlight_vertical_seam(pixels, vertical_seam)
 write_array_into_image(highlight_pixels, "#{filename}-highlighted.jpg")
